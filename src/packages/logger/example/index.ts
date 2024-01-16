ab5f2fb0b4d3cfb074fbdaa7b96b4b817fea69e0
whymind <>
2024-01-16 22:31:35 +0300

    0.0.1

---
+++ src/packages/logger/example/index.ts
@@ -0,0 +1,11 @@
+import { Logger } from "@icord/logger";
+
+const logger = new Logger("ClassName");
+
+logger.log("Log");
+logger.error("Error");
+logger.warn("Warn");
+logger.success("Success");
+logger.verbose("Verbose");
+logger.event("Event");
+logger.debug("Debug");
